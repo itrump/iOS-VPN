e63d513a4c79b1fa54f0aa17e509da394ca170a7
朱炳程 <cheng4741@qq.com>
2016-03-01 11:20:06 +0800

    创建VPN描述文件

--- VPN/ViewController.m
+++ VPN/ViewController.m
@@ -18,7 +18,98 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
+    [self createVPNProfile];
+    
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(VPNStatusDidChangeNotification) name:NEVPNStatusDidChangeNotification object:nil];
+}
+
+- (void)createVPNProfile
+{
+    [[NEVPNManager sharedManager] loadFromPreferencesWithCompletionHandler:^(NSError *error) {
+        
+        if (error) {
+            NSLog(@"Load config failed [%@]", error.localizedDescription);
+            return;
+        }
+        
+        if ([NEVPNManager sharedManager].protocolConfiguration) {
+            // config exists
+        }
+        
+        [self setupIPSec];
+        [[NEVPNManager sharedManager] saveToPreferencesWithCompletionHandler:^(NSError *error) {
+            NSLog(@"Save config failed [%@]", error.localizedDescription);
+        }];
+        
+    }];
+}
+
+- (void)removeVPNProfile
+{
+    [[NEVPNManager sharedManager] loadFromPreferencesWithCompletionHandler:^(NSError *error){
+        if (!error)
+        {
+            [[NEVPNManager sharedManager] removeFromPreferencesWithCompletionHandler:^(NSError *error){
+                if(error)
+                {
+                    NSLog(@"Remove error: %@", error);
+                }
+                else
+                {
+                    NSLog(@"removeFromPreferences");
+                }
+            }];
+        }
+    }];
+    
+}
+
+- (void)setupIPSec
+{
+    // config IPSec protocol
+    NEVPNProtocolIPSec *p = [[NEVPNProtocolIPSec alloc] init];
+    p.username = @"i.vpno.net";
+    p.serverAddress = @"45.79.92.249";
+
+    // get password persistent reference from keychain
+    NSString *password = @"i.vpno.net";
+    NSData *paswordData = [password dataUsingEncoding:NSUTF8StringEncoding];
+    p.passwordReference = paswordData;
+    
+    // PSK
+    p.authenticationMethod = NEVPNIKEAuthenticationMethodSharedSecret;
+    NSString *secret = @"919QqO8F";
+    NSData *secretData = [secret dataUsingEncoding:NSUTF8StringEncoding];
+    p.sharedSecretReference = secretData;
+    
+    
+    p.useExtendedAuthentication = NO;
+    p.disconnectOnSleep = NO;
+    
+    [NEVPNManager sharedManager].protocolConfiguration = p;
+    [NEVPNManager sharedManager].localizedDescription = @"VPN by zhubch";
+}
+
+- (void)connect
+{
+    [[NEVPNManager sharedManager] loadFromPreferencesWithCompletionHandler:^(NSError *error){
+        if (!error)
+        {
+            //配置IPSec
+            [self setupIPSec];
+            [[NEVPNManager sharedManager].connection startVPNTunnelAndReturnError:nil];
+        }
+    }];
+}
+
+- (void)disconnect
+{
+    [[NEVPNManager sharedManager] loadFromPreferencesWithCompletionHandler:^(NSError *error){
+        if (!error)
+        {
+            [[NEVPNManager sharedManager].connection stopVPNTunnel];
+        }
+    }];
 }
 
 #pragma mark - VPN状态切换通知
